2fbfe3935a8e8b0dbad1df17bd1516088494a60a
orblazer <orblazer.minecraft@gmail.com>
2022-01-17 19:50:51 +0100

    feat(script): apply renamed class automatically

--- src/scripts/diff/index.js
+++ src/scripts/diff/index.js
@@ -1,31 +1,95 @@
-const { createTwoFilesPatch } = require('diff')
+const { readFile, writeFile } = require('fs/promises')
 const { resolve } = require('path')
-const { mkdir, writeFile } = require('fs/promises')
-const { downloadFile } = require('./utils')
+const Utils = require('./utils')
 
 const HASHES = {
   last: null,
   current: '532.0f664c4fa9f722e57200',
 }
+const files = [
+  'components/badges-icon.css',
+  'components/button.css',
+  'components/form.css',
+  'components/guild-messages.css',
+  'components/other.css',
+  'components/popout.css',
+  'components/settings.css',
+  'components/better-discord.css',
+]
 
-const cacheDir = resolve(__dirname, '..', '..', '..', '.cache')
+const srcDir = resolve(__dirname, '..', '..')
 ;(async () => {
-  // Create dir if not exist
-  await mkdir(cacheDir, { recursive: true })
+  // Extract classes
+  console.log('[extract] Extracting classes...')
+  const oldClasses = HASHES.last !== null ? await Utils.extractFromHash(HASHES.last) : null
+  const newClasses = await Utils.extractFromHash(HASHES.current)
+  console.log('[extract] Extracted!')
 
-  // Download files
-  const oldCss = HASHES.last !== null ? await downloadFile(HASHES.last + '.css', cacheDir) : null
-  const newCss = await downloadFile(HASHES.current + '.css', cacheDir)
-
-  if (oldCss === null) {
+  if (oldClasses === null) {
     console.log("CAN'T DIFF, NO OLD CSS!")
     return
   }
 
-  // Generate diff
-  await writeFile(
-    resolve(cacheDir, '_diff.patch'),
-    createTwoFilesPatch(HASHES.last + '.css', HASHES.current + '.css', oldCss, newCss)
+  // Diff classes
+  console.log('[diff] Generating diff...')
+  const diff = Utils.diff(oldClasses, newClasses)
+  console.log('[diff] Generated!')
+
+  const removedClasses = new Set()
+  const changedClasses = new Map()
+
+  await Promise.all(
+    files.map(async (file) => {
+      console.log(`[diff] Applying to ${file}...`)
+
+      const filepath = resolve(srcDir, file)
+      let content = await readFile(filepath, 'utf-8')
+      const classes = Utils.extractFromString(content)
+
+      // Applying diff
+      let changed = false
+      classes.forEach((className) => {
+        // The class is removed
+        if (diff.removed.includes(className)) {
+          removedClasses.add(className)
+        }
+        // The class is renamed
+        else if (diff.renamed.has(className)) {
+          const newClass = diff.renamed.get(className)
+          changedClasses.set(className, newClass)
+
+          content = content.replace(new RegExp(`(.|keyframes )(${className})`, 'ig'), (_, prefix) => prefix + newClass)
+          changed = true
+        }
+      })
+
+      // Update the file
+      if (changed) {
+        await writeFile(filepath, content)
+      }
+    })
   )
-  console.log('DONE!')
+
+  // Store Changes
+  console.log('[change] Output changes...')
+  const filepath = resolve(process.cwd(), 'changes.json')
+  writeFile(
+    filepath,
+    JSON.stringify(
+      {
+        removed: removedClasses.values(),
+        added: diff.added,
+        renamed: mapToObj(changedClasses),
+      },
+      2
+    )
+  )
+  console.log(`[change] Changes as been saved to ${filepath}`)
 })()
+
+function mapToObj(m) {
+  return Array.from(m).reduce((obj, [key, value]) => {
+    obj[key] = value
+    return obj
+  }, {})
+}
--- src/scripts/diff/utils.js
+++ src/scripts/diff/utils.js
@@ -1,48 +1,109 @@
-const { createPatch } = require('diff')
-const { readFile, writeFile } = require('fs/promises')
 const { get: httpGet } = require('https')
-const { resolve } = require('path')
+
+const classRegex = /(?:\.|keyframes )([a-z][a-z0-9-_]+)[{,:]/gim
+
+/**
+ * @typedef {object} DiffResult
+ * @property {Map<string, string>} renamed The renamed classes (keys is old and value is new)
+ * @property {string[]} added The new classes
+ * @property {string[]} removed The removed classes
+ */
 
 module.exports = {
   /**
-   * Download the discord asset file and cache it
-   * @param {string} filename The filename
-   * @param {string} cacheDir The cache dir
-   * @returns {Promise<string>} The file content
+   * Extract list classes from discord css file
+   * @param {string} hash The css hash
+   * @returns {Promise<Set<String>>} The file content
    */
-  async downloadFile(filename, cacheDir) {
-    const file = resolve(cacheDir, filename)
+  async extractFromHash(hash) {
+    return new Promise((resolve, reject) => {
+      httpGet(`https://discord.com/assets/${hash}.css`, (res) => {
+        const result = new Set()
 
-    // Read file
-    return (
-      readFile(file, 'utf-8')
-        // Download file if not exist
-        .catch(
-          () =>
-            new Promise((resolve, reject) => {
-              httpGet('https://discord.com/assets/' + filename, (res) => {
-                let content = ''
-                res
-                  .on('data', (chunk) => {
-                    content += chunk
-                      .toString()
-                      .replace(/\n/, '\n\n')
-                      .replace(/}(\.|@)/gi, '}\n$1')
-                  })
-                  .once('end', async () => {
-                    await writeFile(file, content)
-                    resolve(content)
-                  })
-                  .once('error', reject)
-              })
-                .once('error', reject)
-                .end()
-            })
-        )
+        res
+          .on('data', (chunk) => {
+            extractClasses(chunk, result)
+          })
+          .once('end', () => {
+            // Remove soucemap content
+            result.delete('css')
+            result.delete('map')
+
+            resolve(result)
+          })
+          .once('error', reject)
+      })
+        .once('error', reject)
+        .end()
+    })
+  },
+  extractFromString: extractClasses,
+
+  /**
+   * Check the change between two classes list
+   * @param {Set<string>} oldSet The old classes list
+   * @param {Set<string>} newSet The new classes list
+   * @returns {DiffResult} The Change
+   */
+  diff(oldSet, newSet) {
+    const oldClasses = [...oldSet]
+
+    const result = {
+      renamed: new Map(),
+      removed: [],
+      added: [],
+    }
+
+    let name, oldIndex, oldClass
+    newSet.forEach(
+      /** @param {string} className */ (className) => {
+        // Exclude hash of class name
+        name = className.substring(0, className.indexOf('-'))
+
+        // Search class from name
+        oldIndex = oldClasses.findIndex((val) => name === val.substring(0, val.indexOf('-')))
+
+        // The class is an new
+        if (oldIndex === -1) {
+          result.added.push(className)
+          return
+        }
+
+        oldClass = oldClasses[oldIndex]
+
+        // The class has renamed
+        if (oldClass !== className) {
+          result.renamed.set(oldClass, className)
+        }
+
+        // Remove old class
+        oldClasses.splice(oldIndex, 1)
+      }
     )
-  },
 
-  async generatePatch(oldContent, newContent) {
-    createPatch()
+    // Store old classes
+    result.removed = oldClasses
+
+    return result
   },
+}
+
+/**
+ * Extract classes and put in set
+ * @param {string} string The string content
+ * @param {Set<string>} set The result of classes
+ * @returns The passed set
+ */
+function extractClasses(string, set = new Set()) {
+  let match
+  while ((match = classRegex.exec(string)) != null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (match.index === classRegex.lastIndex) {
+      classRegex.lastIndex++
+    }
+
+    set.add(match[1])
+  }
+
+  return set
 }
