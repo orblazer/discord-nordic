18bea0230d8ac68631fd0c1e2ece804ec1785ea5
orblazer <orblazer.minecraft@gmail.com>
2022-11-17 22:15:21 +0100

    refactor: update to last discord version
    This also refactor to better maintain code
    
    This close #14

--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,123 +1,53 @@
-/**
- * Variables
- */
-const pkg = require('../../package.json')
+import { minify } from 'csso'
+import { readFile } from 'fs/promises'
+import { dirname, join, resolve } from 'path'
+import sass from 'sass'
+import { fileURLToPath } from 'url'
+import { betterDiscord, powercord, stylus } from './output.js'
 
-const discordHeader = `/**
- * @name        Nordic
- * @author      orblazer#9152
- * @version     ${pkg.version}
- * @description ${pkg.description}
- * @source      ${pkg.repository}
- * @donate      ${pkg.funding}
- * @authorId    179681974879911946
-*/`
-
-const header = `/**
- * name: Nordic
- * author: orblazer#9152
- * version: ${pkg.version}
-*/`
-
-const stylusHeader = `/* ==UserStyle==
-@name        Discord Nordic
-@namespace   ${pkg.repository}
-@homepageURL ${pkg.repository}
-@version     ${pkg.version}
-@license     CC BY-NC-SA 4.0
-@description ${pkg.description}
-@author      ${pkg.author}
-@supportURL  ${pkg.bugs.url}
-@updateURL   https://raw.githubusercontent.com/orblazer/discord-nordic/master/nordic.user.css
-==/UserStyle== */`
-
-const powercordManifest = {
-  version: pkg.version,
-  name: 'Nordic',
-  description: pkg.description,
-  author: 'orblazer#9152',
-  license: 'CC BY-NC-SA 4.0',
-  theme: 'nordic.theme.css',
-}
-
-const files = [
-  '_variables.css',
-  'components/badges-icon.css',
-  'components/button.css',
-  'components/form.css',
-  'components/guild-messages.css',
-  'components/other.css',
-  'components/popout.css',
-  'components/settings.css',
-  'components/hljs.css',
-  'components/better-discord.css',
-]
-
-/**
- * CODE AREA
- * /!\ DON'T TOUCH ! /!\
- */
-
-// Imports
-const { lstatSync, readFileSync, writeFileSync } = require('fs')
-const { resolve } = require('path')
-const csso = require('csso')
-
+const __filename = dirname(fileURLToPath(import.meta.url))
+const srcDir = resolve(__filename, '..')
 const dev = typeof process.env.DEV === 'undefined' ? false : Boolean(process.env.DEV)
-const srcDir = resolve(__dirname, '..')
 
-// Merge files
-let content = ''
-files.forEach((file) => {
-  const filePath = resolve(srcDir, file)
-  if (lstatSync(filePath).isFile()) {
-    content += readFileSync(filePath).toString()
+// Compile sass files
+const results = await Promise.allSettled([
+  compile('styles/_imports.scss'),
+  compile('styles/plugins/better-discord/_imports.scss'),
+])
+
+// Check if error
+let success = true
+for (const result of results) {
+  if (result.status === 'rejected') {
+    console.error(result.reason)
+    success = false
+    break
   }
-})
-
-// Minify and optimize
-const result = csso.minify(content).css
-
-// Create better discord files
-writeFileSync(
-  resolve('base.css'),
-  `${header}
-${result}`
-)
-writeFileSync(
-  resolve('nordic.theme.css'),
-  `${discordHeader}
-
-/**
- * SOURCE CODE
- * /!\\ DON'T TOUCH ! /!\\
- */
-${dev ? result : "@import url('https://orblazer.github.io/discord-nordic/base.css');"}
-
-/**
- * CUSTOMIZATION
- */
-:root {
 }
 
-.theme-dark {
+if (success) {
+  await Promise.allSettled([
+    betterDiscord([results[0].value, results[1].value].join('\n')),
+    stylus(results[0].value),
+    powercord(results[0].value),
+  ])
 }
 
-.theme-light {
-}`
-)
-// Create stylus file
-writeFileSync(
-  resolve('nordic.user.css'),
-  `${stylusHeader}
-
 /**
- * SOURCE CODE
- * /!\\ DON'T TOUCH ! /!\\
+ * Compile SASS files
+ * @param {string} entry Entry file
  */
-@-moz-document domain("discord.com") {
-  ${result}
-}`
-)
-// Create powercord file
-writeFileSync(resolve('powercord_manifest.json'), JSON.stringify(powercordManifest, null, 2))
+async function compile(entry) {
+  const result = await sass.compileAsync(join(srcDir, entry), {
+    style: dev ? 'expanded' : 'compressed',
+    functions: {
+      async 'svg($path)'([pathArg]) {
+        const path = pathArg.assertString('path').text
+        const file = await readFile(join(srcDir, 'assets', path), { encoding: 'base64' })
+        return new sass.SassString(`data:image/svg+xml;base64,${file}`)
+      },
+    },
+  })
+
+  return dev ? result.css : minify(result.css).css
+}
---
+++ src/scripts/output.js
@@ -0,0 +1,113 @@
+import { readFile, writeFile } from 'fs/promises'
+import { resolve } from 'path'
+
+const pkg = JSON.parse(await readFile(resolve('package.json')))
+
+export function betterDiscord(css) {
+  return writeFile(
+    resolve('nordic.theme.css'),
+    `/**
+ * @name        Nordic
+ * @author      orblazer#9152
+ * @authorId    179681974879911946
+ * @version     ${pkg.version}
+ * @description ${pkg.description}
+ * @source      ${pkg.repository}
+ * @donate      ${pkg.funding}
+ */
+/**
+ * SOURCE CODE
+ * /!\\ DON'T TOUCH ! /!\\
+ */
+${css}
+
+/**
+ * CUSTOMIZATION
+ */
+:root {
+}
+.theme-dark {
+}
+.theme-light {
+}`
+  )
+}
+
+export function stylus(css) {
+  return writeFile(
+    resolve('nordic.user.css'),
+    `/* ==UserStyle==
+@name        Discord Nordic
+@namespace   ${pkg.repository}
+@homepageURL ${pkg.repository}
+@version     ${pkg.version}
+@license     CC BY-NC-SA 4.0
+@description ${pkg.description}
+@author      ${pkg.author}
+@supportURL  ${pkg.bugs.url}
+@updateURL   https://raw.githubusercontent.com/orblazer/discord-nordic/master/nordic.user.css
+==/UserStyle== */
+
+@-moz-document domain("discord.com") {
+  /**
+   * SOURCE CODE
+   * /!\\ DON'T TOUCH ! /!\\
+   */
+  ${css}
+
+  /**
+   * CUSTOMIZATION
+   */
+  :root {
+  }
+  .theme-dark {
+  }
+  .theme-light {
+  }
+}`
+  )
+}
+
+export async function powercord(css) {
+  await writeFile(
+    resolve('nordic.powercord.css'),
+    `/**
+ * @name        Nordic
+ * @author      orblazer#9152
+ * @version     ${pkg.version}
+ * @description ${pkg.description}
+ * @source      ${pkg.repository}
+ * @donate      ${pkg.funding}
+ */
+/**
+ * SOURCE CODE
+ * /!\\ DON'T TOUCH ! /!\\
+ */
+${css}
+
+/**
+ * CUSTOMIZATION
+ */
+:root {
+}
+.theme-dark {
+}
+.theme-light {
+}`
+  )
+  await writeFile(
+    resolve('powercord_manifest.json'),
+    JSON.stringify(
+      {
+        version: pkg.version,
+        name: 'Nordic',
+        description: pkg.description,
+        author: 'orblazer#9152',
+        license: 'CC BY-NC-SA 4.0',
+        theme: 'nordic.powercord.css',
+      },
+      null,
+      2
+    )
+  )
+}
